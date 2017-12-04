1a7c8881081678dab1a144814f68d8ea4ebfd7ef
shihuade <shihuadesysu@gmail.com>
2017-12-04 15:42:46 +0800

    update

--- InputArgSplitTest/FFCommand.m
+++ InputArgSplitTest/FFCommand.m
@@ -41,44 +41,44 @@ char** str_split(char* a_str, const char a_delim, int *retCount)
         
         if ((0 == iDQuoMkNum % 2) && (0 == iSQuoMkNum % 2) && a_delim == *tmp)
         {
-//            if (!preDelim) {
-//                count++;
-//                preDelim = 1;
-//            }
-            printf("space  %c\n", *tmp);
-
-            count++;
+            if (!preDelim) {
+                count++;
+                preDelim = 1;
+                printf("space  %c\n", *tmp);
+            }
             last_comma = tmp;
-//        } else {
-//            preDelim = 0;
-//        }
+        } else {
+            preDelim = 0;
+        
         }
         tmp++;
     }
     
-    /* Add space for trailing token. */
-//    count += last_comma < (a_str + strlen(a_str) - 1);
+    //Add space for trailing token.
+    count += last_comma < (a_str + strlen(a_str) - 1);
     
-//    /* Add space for terminating null string so caller
-//     knows where the list of returned strings ends. */
-//
-//    result = malloc(sizeof(char*) * count);
-//    memset(result, 0, sizeof(char*) * count);
-//
-//    if (result) {
-//        char* token = strtok(a_str, delim);
-//
-//        while (token) {
-//            *(result + idx++) = strdup(token);
-//            token = strtok(0, delim);
-//        }
-//    }
-//
-//    *retCount = (int)count;
-//
-    NSLog(@"ffmpeg command params count = %d idx = %d", (int)count, (int)idx);
-//    NSLog(@"ffmpeg command after:  %s", *result);
+    /* Add space for terminating null string so caller
+     knows where the list of returned strings ends. */
 
+    result = malloc(sizeof(char*) * count);
+    memset(result, 0, sizeof(char*) * count);
+
+    if (result) {
+        char* token = strtok(a_str, delim);
+
+        while (token) {
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+    }
+
+    *retCount = (int)count;
+
+    NSLog(@"ffmpeg command params count = %d idx = %d", (int)count, (int)idx);
+    for(int i=0; i<=count; i++) {
+      NSLog(@"ffmpeg command after:  %s", *result);
+        result++;
+    }
     return result;
 }
 
