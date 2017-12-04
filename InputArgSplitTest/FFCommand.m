3cea19bc29716ac45bfae2487508e1c5074d3979
Edward Shi <shihuade@users.noreply.github.com>
2017-12-04 14:05:55 +0800

    update

--- InputArgSplitTest/FFCommand.m
+++ InputArgSplitTest/FFCommand.m
@@ -23,46 +23,61 @@ char** str_split(char* a_str, const char a_delim, int *retCount)
     
     /* Count how many elements will be extracted. */
     int preDelim = 0;
-    int iQuoMkNum = 0;
+    int iDQuoMkNum = 0;
+    int iSQuoMkNum = 0;
+
     NSLog(@"ffmpeg command before:  %s", a_str);
 
     while (*tmp)
     {
-        if (a_delim == *tmp && (0 == iQuoMkNum % 2))
+        if ('\"' == *tmp) {
+            iDQuoMkNum ++;
+            printf("double quo %c\n", *tmp);
+        }
+        if ('\'' == *tmp) {
+            iSQuoMkNum ++;
+            printf("single quo %c\n", *tmp);
+        }
+        
+        if ((0 == iDQuoMkNum % 2) && (0 == iSQuoMkNum % 2) && a_delim == *tmp)
         {
-            if (!preDelim) {
-                count++;
-                preDelim = 1;
-            }
+//            if (!preDelim) {
+//                count++;
+//                preDelim = 1;
+//            }
+            printf("space  %c\n", *tmp);
+
+            count++;
             last_comma = tmp;
-        } else {
-            preDelim = 0;
+//        } else {
+//            preDelim = 0;
+//        }
         }
         tmp++;
     }
     
     /* Add space for trailing token. */
-    count += last_comma < (a_str + strlen(a_str) - 1);
+//    count += last_comma < (a_str + strlen(a_str) - 1);
     
-    /* Add space for terminating null string so caller
-     knows where the list of returned strings ends. */
-    
-    result = malloc(sizeof(char*) * count);
-    memset(result, 0, sizeof(char*) * count);
-    
-    if (result) {
-        char* token = strtok(a_str, delim);
-        
-        while (token) {
-            *(result + idx++) = strdup(token);
-            token = strtok(0, delim);
-        }
-    }
-    
-    *retCount = (int)count;
-    
+//    /* Add space for terminating null string so caller
+//     knows where the list of returned strings ends. */
+//
+//    result = malloc(sizeof(char*) * count);
+//    memset(result, 0, sizeof(char*) * count);
+//
+//    if (result) {
+//        char* token = strtok(a_str, delim);
+//
+//        while (token) {
+//            *(result + idx++) = strdup(token);
+//            token = strtok(0, delim);
+//        }
+//    }
+//
+//    *retCount = (int)count;
+//
     NSLog(@"ffmpeg command params count = %d idx = %d", (int)count, (int)idx);
-    NSLog(@"ffmpeg command after:  %s", *result);
+//    NSLog(@"ffmpeg command after:  %s", *result);
 
     return result;
 }
